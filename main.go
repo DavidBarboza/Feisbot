7a64802c8861c18e09c2c476781e809c8ab41d62
davidbarboza <benjigoenitz@gmail.com>
2017-11-02 23:57:45 -0600

    setup web project

--- main.go
+++ main.go
@@ -1,1 +1,50 @@
-package Feisbot
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+	Port    string `json:"port"`
+	CertPem string `json:"cert_pem"`
+	KeyPem  string `json:"key_pem"`
+}
+
+var config Config
+
+func main() {
+	loadConfig()
+
+	http.HandleFunc("/", saludar)
+
+	log.Printf("servidor iniciado en https://localhost%s", config.Port)
+	err := http.ListenAndServeTLS(config.Port,
+					config.CertPem,
+					config.KeyPem,
+					nil)
+	if err != nil {
+		log.Println(err)
+	}
+	//log.Println(http.ListenAndServe(":8080", nil))
+}
+
+func saludar(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+func loadConfig() {
+	log.Println("Loading configuration file...")
+	b, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		log.Fatalf("Error converting config file: %v", err)
+	}
+	log.Println("Configuration loaded")
+}
